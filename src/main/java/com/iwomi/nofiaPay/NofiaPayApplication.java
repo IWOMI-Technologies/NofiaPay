15479f8c3f5ca0cd2b39abe0ffee702c2c9ac1db
mikeito <42328149+mikeito@users.noreply.github.com>
2024-07-29 19:47:21 +0100

    first commit

---
+++ src/main/java/com/iwomi/nofiaPay/NofiaPayApplication.java
@@ -0,0 +1,13 @@
+package com.iwomi.nofiaPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NofiaPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NofiaPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iwomi/nofiaPay/NofiaPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwomi.nofiaPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NofiaPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
