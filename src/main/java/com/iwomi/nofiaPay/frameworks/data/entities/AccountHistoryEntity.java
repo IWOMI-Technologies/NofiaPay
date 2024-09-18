bc2c70493ccdb2c0a22e79b0fb2d2f412093b6af
Nji-bran2 <108367035+Nji-bran2@users.noreply.github.com>
2024-09-18 10:57:38 +0100

    Update AccountHistoryEntity.java

--- src/main/java/com/iwomi/nofiaPay/frameworks/data/entities/AccountHistoryEntity.java
+++ src/main/java/com/iwomi/nofiaPay/frameworks/data/entities/AccountHistoryEntity.java
@@ -35,7 +35,6 @@ public class AccountHistoryEntity extends BaseEntity {
     private String balance;
 
 
-    @Enumerated(EnumType.STRING) private StatusTypeEnum status;
     @Enumerated(EnumType.STRING) private OperationTypeEnum type;
 
 }
